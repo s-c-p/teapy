35222aed7fc4c81e26dcef04c37b0ec80733947a
s-c-p <prasadsatish45@binkmail.com>
2018-02-18 17:12:08 +0530

    test

---
+++ tea/test.py
@@ -0,0 +1,25 @@
+from tea.msgFactory import msgType
+from tea.switch_case import switch
+
+msgType('Inc', 'vsra', globals(), {})
+msgType('Dec', '2sra', globals(), {})
+
+y = Dec()
+
+def f(x):
+	with switch(x, locals(), globals()) as (case, default):
+		@case(Inc)
+		def _():
+			return "+"
+		@case(Dec)
+		def _():
+			return "-"
+		@default
+		def _():
+			return "ha ha"
+	return locals()['switch_case_result']
+
+assert f(Inc) == "ha ha"
+assert f(Inc()) == "+"
+assert f(Dec()) == "-"
+
