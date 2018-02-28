20a19f10e9b61907ea649b88c53e0a9daaf6f7cc
s-c-p <prasadsatish45@binkmail.com>
2018-02-28 15:51:40 +0530

    get this to work

--- tea/programs.py
+++ tea/programs.py
@@ -1,11 +1,45 @@
+import time
+import asyncio as aio
+
+def f(x):
+	time.sleep(1)
+	print("f says: " + str(x*2))
+	return x*2
+
+def s(x):
+	time.sleep(3)
+	print("s says: " + str(x*2))
+	return x**2
+
+x = 10
+
+f = aio.coroutine(f)
+s = aio.coroutine(s)
+
+assert aio.iscoroutinefunction(f)
+assert aio.iscoroutinefunction(s)
+
+futures = {f(x), s(x)}
+
+def executor():
+	yield from aio.wait(futures, return_when=aio.FIRST_COMPLETED)
+
+msg, _ = executor()
+
 class WhoeverSpeaksFirst(object):
     def __init__(self, *args):
         self.funcs = list(args)
 
     def given(self, *args):
         for aFunc in self.funcs:
-            async aFunc(*args)
         return ans
+
+
+
+
+
+
+
 
 def program(initTuple, viewFn, updateFn, subscriptionFn):
     msgType = foreignContext['Msg']
