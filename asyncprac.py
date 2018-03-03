7807b8ff4a2e4de16082356ef65588c67a183f1b
s-c-p <prasadsatish45@binkmail.com>
2018-03-03 18:58:44 +0530

    good sources

--- asyncprac.py
+++ asyncprac.py
@@ -1,19 +1,79 @@
-import bottle
+''' See:
+https://pymotw.com/3/asyncio/executors.html
+https://github.com/calebmadrigal/asyncio-examples/blob/master/
+    run_in_executor.py
+    call_sync_code.py
+    concurrent_blocking_requests.py
+    parallel_examples.py
+'''
 
+import os
+import time
+import random
+import asyncio
 
-@bottle.route('/')
-def index():
-    return bottle.static_file('client.html', root='.')
+random.seed(os.urandom(512))
 
-@bottle.route('/r1')
-def ndf1():
-    print('message from 1')
-    return
+def update(msg, model):
+    return 'update'
 
-@bottle.route('/r2')
-def ndf2():
-    print('message from 2')
-    return
+def view(model):
+    x = random.random()*2
+    time.sleep(x)
+    return 0
+
+def subscriptions(model):
+    x = random.random()*2
+    time.sleep(x)
+    return 0
+
+import sys
+import logging
+from functools import wraps
+from concurrent.futures import ThreadPoolExecutor
+
+def l_deco(func):
+    @wraps(func)
+    def wrapper(*args):
+        log = logging.getLogger(func.__name__)
+        log.info('running')
+        func(*args)
+        log.info('done')
+    return wrapper
+
+async def run_blking_func(executor):
+    log = logging.getLogger('run_blking_func')
+    log.info('starting')
+
+    log.info('creating executor tasks')
+    loop = asyncio.get_event_loop()
+    blking_tasks = [
+        loop.run_in_executor(executor, l_deco(view), 0),
+        loop.run_in_executor(executor, l_deco(subscriptions), 0)
+    ]
+    log.info('waiting fro executor class')
+    completed, pending = await asyncio.wait(blking_tasks)
+    results = [t.result() for t in completed]
+    log.info('results: {!r}'.format(results))
+
+    log.info('exiting')
+
+def pymotw():
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(threadName)10s %(name)18s: %(message)s',
+        stream=sys.stderr
+    )
+
+    executor = ThreadPoolExecutor(max_workers=3)
+
+    el = asyncio.get_event_loop()
+    try:
+        el.run_until_complete(  run_blking_func(executor)  )
+    finally:
+        el.close()
 
 if __name__ == '__main__':
-    bottle.run(host='localhost', port=8080)#, debug=True)
+    pymotw()
+
+
