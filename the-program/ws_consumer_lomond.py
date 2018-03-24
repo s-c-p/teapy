37667f3ac0943e221887df18357de8963f3a19bf
s-c-p <prasadsatish45@binkmail.com>
2018-03-24 22:58:32 +0530

    run experiment in ...lomond.py and feel the need of trio/curio

--- the-program/ws_consumer_lomond.py
+++ the-program/ws_consumer_lomond.py
@@ -1,33 +1,35 @@
-import time
-
 from lomond import WebSocket
 
-def get_input(text=''):
-    return input(text)
-
-name = get_input("your name: ")
-ws = WebSocket('ws://echo.websocket.org/')
+name = input("your name: ")
+ws = WebSocket("ws://127.0.0.1:5678/")
 
 def simple_run():
     for event in ws:
-        print(event)
-        if event.name == 'poll':
-            print('sending data @' + str(time.monotonic()))
-            ws.send_text("<{} connected>".format(name))
-        elif event.name == 'text':
-            print(time.monotonic(), event.text)
+        # if event.name == 'poll':
+        #     print('sending data @' + str(time.monotonic()))
+        #     ws.send_text("<{} connected>".format(name))
+        # elif event.name == 'text':
+        if event.name == 'text':
+            print(event.text)
 
 def threaded():
-    from threading import Thread
-    Thread(target=simple_run).start()
-    while True:
-        try:
-            ws.send_text("[{}] {}".format(name, get_input()))
-        except KeyboardInterrupt:
-            ws.close()
-            break
+    try:
+        simple_run()
+    except KeyboardInterrupt:
+        ws.close()
     return
 
+def experiment():
+    import time
+    ibl = iter(ws)
+    while True:
+        try:
+            print(time.time(), next(ibl))
+        except KeyboardInterrupt:
+            break
+    ws.close()
+
 if __name__ == '__main__':
-    threaded()
+    # threaded()
+    experiment()
 
--- the-program/ws_factory.py
+++ the-program/ws_factory.py
@@ -9,7 +9,7 @@ async def time(websocket, path):
     while True:
         now = datetime.datetime.utcnow().isoformat() + 'Z'
         await websocket.send(now)
-        await asyncio.sleep(random.random() * 3)
+        await asyncio.sleep(random.random() * 9)
 
 start_server = websockets.serve(time, '127.0.0.1', 5678)
 
