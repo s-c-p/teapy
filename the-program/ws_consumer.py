6f540b8dc188562544919f8b385ae4bd6f877d95
s-c-p <prasadsatish45@binkmail.com>
2018-03-25 18:40:50 +0530

    gotta run em both at the same time

--- the-program/ws_consumer.py
+++ the-program/ws_consumer.py
@@ -1,4 +1,4 @@
-from lomond
+import lomond
 
 name = input("your name: ")
 ws = lomond.WebSocket("ws://127.0.0.1:5678/")
@@ -29,28 +29,57 @@ def experiment():
             break
     ws.close()
 
-class TrioWebSocket(object):
-    def __init__(self, ws_addr):
-        self.ws_addr = ws
+# class TrioWebSocket(object):
+#     def __init__(self, ws_addr):
+#         self.ws_addr = ws
+# 
+#     def __aiter__(self):
+#         self.ws = lomond.WebSocket(self.ws_addr)
+#         for event in self.ws:
+#             if isinstance(event, lomond.events.Ready):
+#                 return self
+# 
+#     async def __anext__(self):
+#         i = iter(self.ws)
+#         while True:
+#             try:
+#                 incm = next(i)
+#             except:
+#                 raise StopAsyncIteration
+#             else:
+#                 return incm
+#             finally:
+#                 self.ws.close()
+#         return
+# 
+# async def async_ws(addr):
+#     trio_ws = TrioWebSocket(addr)
+#     while True:
+#         ans = await trio_ws.__anext__()
 
-    def __aiter__(self):
-        self.ws = lomond.WebSocket(self.ws_addr)
-        for event in self.ws:
-            if isinstance(event, lomond.events.Ready):
-                return self
-
-    async def __anext__(self):
-        i = iter(self.ws)
+def ws_coro(ws_addr, init_msg=None):
+    ws = lomond.WebSocket(ws_addr)
+    ans = init_msg
+    step = int()
+    ws_iter = iter(ws)
+    while True:
+        if step > 5:
+            raise RuntimeError("Websocket error")
+        event = next(ws_iter)
+        if isinstance(event, lomond.events.Ready):
+            break
+        else:
+            step += 1
+    try:
         while True:
-            try:
-                incm = next(i)
-            except:
-                raise StopAsyncIteration
+            if ans == None:
+                continue
             else:
-                return incm
-            finally:
-                self.ws.close()
-        return
+                ws.send_text(ans)
+            ans = yield next(ws_iter)
+    except:
+        ws.close()
+
 
 if __name__ == '__main__':
     # threaded()
